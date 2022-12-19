7bc52baff44387c994d2003b361cb7e414840649
Abhishek <abhishekg@witmates.com>
2022-12-19 18:50:16 +0530

    Add Category Filter

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,8 +3,15 @@ class HomeController < ApplicationController
   def index
     respond_to do |format|
       format.html {
-        @products = Product.where("LOWER(name) LIKE ? OR price LIKE ?", params[:search.downcase] + "%", params[:search] + "%") if params[:search].present?
-        @products = Product.all if !params[:search].present?
+        #@products = Product.where("LOWER(name) LIKE ? OR price LIKE ?", params[:search.downcase] + "%", params[:search] + "%") if params[:search].present?
+        #@products = Product.all if !params[:search].present?
+        @products = if params[:search].present?
+                      Product.where("LOWER(name) LIKE ? OR price LIKE ?", params[:search.downcase] + "%", params[:search] + "%") 
+                    elsif params[:category_id].present?
+                      Product.where(category_id: params[:category_id]) if params[:category_id].present?
+                    else
+                      Product.all
+                    end
         if current_user.present?
           @email = current_user.email
         end
